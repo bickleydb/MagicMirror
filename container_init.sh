8bf2c6d06193cbc7ea0189cb84996855625300cb
Daniel <bickleydaniel@gmail.com>
2018-12-05 21:03:20 -0800

    Forgot a couple update

--- container_init.sh
+++ container_init.sh
@@ -18,6 +18,9 @@ for filename in $(find -name *.webpack.config.js); do
    webpack --config $filename
 done
 
+cd /www/MagicMirror/MagicMirror/MagicMirror
+cat settings.py linux_settings.txt > temp.out; cp temp.out settings.py
+
 cd /www/MagicMirror/MagicMirror
 python3 ./manage.py collectstatic
 
