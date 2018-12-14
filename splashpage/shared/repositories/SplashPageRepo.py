3b874a50b43b873055b2d0a4a6c779ae81d8a8db
Daniel <bickleydaniel@gmail.com>
2018-12-13 21:14:08 -0800

    Fix more PEP8 stuff

--- splashpage/shared/repositories/SplashPageRepo.py
+++ splashpage/shared/repositories/SplashPageRepo.py
@@ -1,6 +1,6 @@
 import splashpage.models.SplashPageModel as SplashPageModel
 
+
 class SplashPageRepo:
     def load_splash_page(self):
         return SplashPageModel.SplashPageModel.get_manager().all()[0]
-        
