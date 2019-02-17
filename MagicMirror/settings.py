ee14acb96ac33cad5245f5a5774af07c66de793f
Daniel <bickleydaniel@gmail.com>
2019-02-16 23:26:38 -0800

    Trying to fix allowed hosts

--- MagicMirror/settings.py
+++ MagicMirror/settings.py
@@ -25,12 +25,7 @@ SECRET_KEY = os.environ["djangoSecret"] or ""
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-    os.environ["ipAddr"]
-]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', os.environ["ipAddr"]]
 
 # Application definition
 
