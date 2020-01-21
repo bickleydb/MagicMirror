9b4afcfd2b06ff4ce4d7a97016d79e483b97a05c
Daniel <bickleydaniel@gmail.com>
2020-01-20 16:26:17 -0800

    Prevent crashing if env vars aren't defined

--- statements/shared/twitter_adapter/__init__.py
+++ statements/shared/twitter_adapter/__init__.py
@@ -3,10 +3,33 @@ import os
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-CONSUMER_KEY = os.environ["TwitterConsumerKey"]
-CONSUMER_SECRET = os.environ["TwitterConsumerSecret"],
-ACCESS_TOKEN_KEY = os.environ["TwitterAccessTokenKey"],
-ACCESS_TOKEN_SECRET = os.environ["TwitterAccessTokenSecret"],
+EnvConfigVars = {
+    "TwitterConsumerKey": "TwitterConsumerKey",
+    "TwitterConsumerSecret": "TwitterConsumerSecret",
+    "TwitterAccessTokenKey": "TwitterAccessTokenKey",
+    "TwitterAccessTokenSecret": "TwitterAccessTokenSecret",
+}
+
+
+if EnvConfigVars["TwitterConsumerKey"] in os.environ:
+    CONSUMER_KEY = os.environ["TwitterConsumerKey"]
+else:
+    CONSUMER_KEY = ""
+
+if EnvConfigVars["TwitterConsumerSecret"] in os.environ:
+    CONSUMER_SECRET = os.environ["TwitterConsumerSecret"]
+else:
+    CONSUMER_SECRET = ""
+
+if EnvConfigVars["TwitterAccessTokenKey"] in os.environ:
+    ACCESS_TOKEN_KEY = os.environ["TwitterAccessTokenKey"]
+else:
+    ACCESS_TOKEN_KEY = ""
+
+if EnvConfigVars["TwitterAccessTokenSecret"] in os.environ:
+    ACCESS_TOKEN_SECRET = os.environ["TwitterAccessTokenKey"]
+else:
+    ACCESS_TOKEN_SECRET = ""
 
 
 def get_consumer_key():
--- weather/shared/__init__.py
+++ weather/shared/__init__.py
@@ -3,6 +3,23 @@ import os
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-APP_KEY = os.environ["WeatherPrivateKey"]
-ZIP_CODE = os.environ["ZipCode"]
-COUNTRY_CODE = os.environ["CountryCode"]
+EnvConfigVars = {
+    "WeatherPrivateKey": "WeatherPrivateKey",
+    "ZipCode": "ZipCode",
+    "CountryCode": "CountryCode",
+}
+
+if EnvConfigVars["WeatherPrivateKey"] in os.environ:
+    APP_KEY = os.environ["WeatherPrivateKey"]
+else:
+    APP_KEY = ""
+
+if EnvConfigVars["ZipCode"] in os.environ:
+    ZIP_CODE = os.environ["ZipCode"]
+else:
+    ZIP_CODE = ""
+
+if EnvConfigVars["CountryCode"] in os.environ:
+    COUNTRY_CODE = os.environ["CountryCode"]
+else:
+    COUNTRY_CODE = ""
